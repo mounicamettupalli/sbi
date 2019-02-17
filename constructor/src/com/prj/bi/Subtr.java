e9b8b165ff3f2e909d55b811359ac14c994d1ca3
mounicamettupalli <mounicamettupalli5@gmail.com>
2019-02-17 14:23:41 -0500

    AAA

--- constructor/src/com/prj/bi/Subtr.java
+++ constructor/src/com/prj/bi/Subtr.java
@@ -4,10 +4,10 @@ public class Subtr {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		
 		System.out.println("This is example");
 		System.out.println("This is example1");
-
 
 	}
 
